9f7ae7311e58b05f64ea631c7484c8062f8edd40
Lars Hagen <mail@laitch.com>
2021-02-05 15:35:41 +0100

    Initial commit

---
+++ Assets/Connection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Connection
+{
+    public ConnectionPoint a;
+    public ConnectionPoint b;
+    public float preferedLength;
+    public float forceMultiplier = 10f;
+
+    public void Evaluate(Dictionary<ConnectionPoint, Vector2> calculatedForces)
+    {
+        float force = CalculateForce();
+        
+        Vector2 direction = (b.transform.position - a.transform.position).normalized;
+
+        Vector2 forceA = default;
+        Vector2 forceB = default;
+
+        if (a.locked)
+        {
+            forceB -= direction * force * 2;
+        }
+        else if (b.locked)
+        {
+            forceA += direction * force * 2;
+        }
+        else
+        {
+            forceA += direction * force;
+            forceB -= direction * force;
+        }
+
+        if (calculatedForces.ContainsKey(a))
+            calculatedForces[a] += forceA;
+        else
+            calculatedForces.Add(a, forceA);
+
+        if (calculatedForces.ContainsKey(b))
+            calculatedForces[b] += forceB;
+        else
+            calculatedForces.Add(b, forceB);
+    }
+
+    private float CalculateForce()
+    {
+        float length = Vector2.Distance(a.transform.position, b.transform.position);
+        return (length - preferedLength) * forceMultiplier;
+    }
+
+    public void DrawDebug()
+    {
+        Debug.Log(CalculateForce());
+        Debug.DrawLine(a.transform.position, b.transform.position, Color.Lerp(Color.blue, Color.red, CalculateForce()));
+    }
+}
---
+++ Assets/ConnectionPoint.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ConnectionPoint : MonoBehaviour
+{
+    [SerializeField] private ConnectionPoint[] connections;
+    [SerializeField] private float gravity = 1f;
+
+    public bool locked;
+
+    public float damping = 10;
+    public Vector2 velocity = new Vector2();
+    public float mass = 1;
+
+    public void ApplyForce(Vector2 connectionsForce)
+    {
+        if (locked)
+            return;
+
+        Vector2 dampingForce = velocity * damping;
+        Vector2 force = connectionsForce + mass * new Vector2(0, gravity) - dampingForce;
+        Vector2 acceleration = force / mass;
+        velocity += acceleration * Time.fixedDeltaTime;
+        transform.position += (Vector3)velocity;
+    }
+
+    private void Awake()
+    {
+        foreach (var connection in connections)
+        {
+            var exsists = ConnectionManager.connections.Any(c => (c.a == this && c.b == connection) || (c.a == connection && c.b == this));
+            if (exsists)
+                continue;
+
+            ConnectionManager.connections.Add(new Connection()
+            {
+                a = this,
+                b = connection,
+                preferedLength = Vector2.Distance(transform.position, connection.transform.position)
+            });
+        }
+    }
+
+}
